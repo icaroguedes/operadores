2455984efb98c800110459951d305fbb49c2f498
Icaro Guedes <134503766+icaroguedes@users.noreply.github.com>
2023-08-22 08:31:13 -0300

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,29 @@
+function Calculavalores(){    
+    var s1 = document.getElementById("valor1").value;
+    var s2 = document.getElementById("valor2").value;
+    var s3 = parseInt(s1) + parseInt(s2);
+    alert(s3);
+}
+
+function Subtrairvalores(){    
+    var s1 = document.getElementById("valor1").value;
+    var s2 = document.getElementById("valor2").value;
+    var s3 = parseInt(s1) - parseInt(s2);
+    alert(s3);
+}
+
+function Multiplicarvalores(){    
+    var s1 = document.getElementById("valor1").value;
+    var s2 = document.getElementById("valor2").value;
+    var s3 = parseInt(s1) * parseInt(s2);
+    alert(s3);
+}
+
+
+function Dividirvalores(){    
+    var s1 = document.getElementById("valor1").value;
+    var s2 = document.getElementById("valor2").value;
+    var s3 = parseInt(s1) / parseInt(s2);
+    alert(s3);
+}
+
